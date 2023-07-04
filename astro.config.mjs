01df70e56f94c6d67ac8c9d20995c9268563060a
tylerbishopdev <tbishop@ezoic.com>
2023-07-03 18:06:58 -0700

    new

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image()],
+  output: "server",
+  adapter: netlify(),
+  
+});
