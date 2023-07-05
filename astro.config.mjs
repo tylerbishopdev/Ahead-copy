696f92b8da44aefdf24861e7a456b80e041ee4c1
tylerbishopdev <tbishop@ezoic.com>
2023-07-05 10:07:45 -0700

    updated deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
@@ -8,6 +8,6 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   integrations: [tailwind(), image()],
   output: "server",
-  adapter: netlify(),
-  
+  adapter: netlify()
+  // adapter: "netlify"
 });
